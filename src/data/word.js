8859643fe85deb4e8f89d54e60e44e194f270bdf
yangdepp <yangdepp@163.com>
2022-10-03 00:43:44 +0800

    done

--- src/data/word.js
+++ src/data/word.js
@@ -3,6 +3,130 @@ export const words = {
     {
       eng: 'emperor',
       zh: '皇帝',
+      type: 'n.',
+      eg: [
+        {
+          eng: 'I was the emperor of China.',
+          zh: '我原是中国的皇帝',
+        },
+      ],
+      root: [
+        {
+          eng: 'emper, imper = command',
+          zh: '前缀: 皇帝有关的',
+        },
+      ],
+      similar: [
+        {
+          eng: 'imperial',
+          zh: '皇帝的，帝国的',
+        },
+        {
+          eng: 'empire',
+          zh: '帝国',
+        },
+        {
+          eng: 'imperious',
+          zh: '盛气凌人的',
+        },
+      ],
+    },
+    {
+      eng: 'emperor1',
+      zh: '皇帝',
+      type: 'n',
+      eg: [
+        {
+          eng: 'I was the emperor of China.',
+          zh: '我原是中国的皇帝',
+        },
+      ],
+      root: [
+        {
+          eng: 'emper / imper = command',
+          zh: '前缀: 皇帝有关的',
+        },
+      ],
+      similar: [
+        {
+          eng: 'imperial',
+          zh: '皇帝的，帝国的',
+        },
+        {
+          eng: 'empire',
+          zh: '帝国',
+        },
+        {
+          eng: 'imperious',
+          zh: '盛气凌人的',
+        },
+      ],
+    },
+    {
+      eng: 'emperor2',
+      zh: '皇帝',
+      type: 'n',
+      eg: [
+        {
+          eng: 'I was the emperor of China.',
+          zh: '我原是中国的皇帝',
+        },
+      ],
+      root: [
+        {
+          eng: 'emper / imper = command',
+          zh: '前缀: 皇帝有关的',
+        },
+      ],
+      similar: [
+        {
+          eng: 'imperial',
+          zh: '皇帝的，帝国的',
+        },
+        {
+          eng: 'empire',
+          zh: '帝国',
+        },
+        {
+          eng: 'imperious',
+          zh: '盛气凌人的',
+        },
+      ],
+    },
+    {
+      eng: 'emperor3',
+      zh: '皇帝',
+      type: 'n',
+      eg: [
+        {
+          eng: 'I was the emperor of China.',
+          zh: '我原是中国的皇帝',
+        },
+      ],
+      root: [
+        {
+          eng: 'emper / imper = command',
+          zh: '前缀: 皇帝有关的',
+        },
+      ],
+      similar: [
+        {
+          eng: 'imperial',
+          zh: '皇帝的，帝国的',
+        },
+        {
+          eng: 'empire',
+          zh: '帝国',
+        },
+        {
+          eng: 'imperious',
+          zh: '盛气凌人的',
+        },
+      ],
+    },
+    {
+      eng: 'emperor4',
+      zh: '皇帝',
       type: 'n',
       eg: [
         {
--- src/page/word/index.js
+++ src/page/word/index.js
@@ -1,12 +1,100 @@
 import './style/index.less';
-import {words} from "../../data/word";
+import { words } from '../../data/word';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Space, Typography, Divider } from 'antd';
+const { Title, Paragraph } = Typography;
 
 console.log(words);
+const { ielts } = words;
 
 const Word = () => {
+  const [number, setNumber] = useState(0);
+
+  const currentIeltsWord = ielts[number];
+
+  useEffect(() => {
+    window.addEventListener('keydown', onKeyDown); // 添加全局事件
+    return () => {
+      window.removeEventListener('keydown', onKeyDown); // 销毁
+    };
+  }, []);
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 39 || e.keyCode === 40) {
+      setNumber((pre) => {
+        console.log(pre, ielts.length);
+        if (pre < ielts.length - 1) {
+          return pre + 1;
+        } else {
+          return pre;
+        }
+      });
+    }
+    if (e.keyCode === 37 || e.keyCode === 38) {
+      setNumber((pre) => {
+        if (pre > 0) {
+          return pre - 1;
+        } else {
+          return pre;
+        }
+      });
+    }
+  };
+
   return (
     <div className="word">
-      <div>Word</div>
+      <div className="current-word">
+        <Space align="baseline" size={20}>
+          <Title level={4}>{currentIeltsWord.eng}</Title>
+          <Paragraph>
+            {currentIeltsWord.type} {currentIeltsWord.zh}
+          </Paragraph>
+          <div>
+            {currentIeltsWord.root.map((item) => {
+              return (
+                <Paragraph>
+                  <pre>
+                    <div className="root">
+                      {item.eng}
+                      <Divider
+                        type="vertical"
+                        style={{ borderColor: '#ffa940' }}
+                      />
+                      {item.zh}
+                    </div>
+                  </pre>
+                </Paragraph>
+              );
+            })}
+          </div>
+        </Space>
+      </div>
+
+      <div>
+        {currentIeltsWord.eg.map((item) => {
+          return (
+            <Paragraph>
+              <pre>
+                <div>{item.eng}</div>
+                <div>{item.zh}</div>
+              </pre>
+            </Paragraph>
+          );
+        })}
+      </div>
+      <div>
+        <ul>
+          {currentIeltsWord.similar.map((item) => {
+            return (
+              <li>
+                <div>{item.eng}</div>
+                <div>{item.zh}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
